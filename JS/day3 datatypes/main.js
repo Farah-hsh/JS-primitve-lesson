c026f66764ddd42935c5030dde9cedd73ce312a3
Farah <farah.hsh.12@gmail.com>
2023-08-08 21:47:57 +0300

    day3

--- JS/day3 datatypes/main.js
+++ JS/day3 datatypes/main.js
@@ -192,6 +192,21 @@ console.log(str.split(" ", 2));
 
 // challenge 2
 
+let str1=" java script language";
+
+console.log(str.length);
+console.log('test',length);
+
+
+console.log(str.charAt(0));
+console.log('abc'.charAt(1));
+console.log(str.slice(0,4));
+console.log('test'.slice(1,3));
+
+
+console.log(str.split(' '));
+console.log('192.168.1.1'.split('.'));
+
 
 
 
